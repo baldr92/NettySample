b6222a6d38782eded56aee71e6d19c19efac01a0
PLatchenkov <baldr1992@gmail.com>
2018-08-06 18:31:11 +0300

    Initial commit

---
+++ src/RequestData.java
@@ -0,0 +1,22 @@
+public class RequestData {
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+
+    private int intValue;
+    private String stringValue;
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    // standard getters and setters
+}
